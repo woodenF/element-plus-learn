b134ecd42cda35d5c86e59d4b64c4ed0bac323a3
hao.feng <hao.feng>
2020-12-23 16:02:09 +0800

    简单input效果

---
+++ src/otherComponents/input/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="other-input">
+    <div class="input-title" :class="{active: inputText || isFocus}">title</div>
+    <input
+      v-model="inputText"
+      @focus="handleFocus"
+      @blur="handleBlur"
+      type="text"
+    >
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'OtherInput',
+  setup() {
+    const isFocus = ref(false)
+    const inputText = ref('')
+
+    const handleFocus = (e: FocusEvent) => {
+      isFocus.value = true
+    }
+
+    const handleBlur = (e: FocusEvent) => {
+      isFocus.value = false
+    }
+    return {
+      handleFocus,
+      handleBlur,
+      isFocus,
+      inputText
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+.other-input{
+  .input-title{
+    pointer-events: none;
+    transition: all .3s;
+    height: 18px;
+    transform: translateY(16px) translateX(2px);
+    color: #ccc;
+    font-size: 14px;
+    &.active{
+      transform: translateX(0) translateY(0);
+      color: #666;
+      font-size: 16px;
+    }
+  }
+  input{
+    background: none;
+    border: none;
+    outline: none;
+    border-bottom: 1px solid #aaa;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,15 +8,32 @@
       :before-close="beforeClose"
     >
     </el-dialog>
+    <other-input></other-input>
+    <other-input></other-input>
+
+    <div
+      class="image-wrapper"
+      :style="{transform: `translateZ(-500px) rotateY(-${360 / imgs.length * 1}deg) rotateZ(45deg)`}"
+    >
+      <img
+        v-for="(item, index) in imgs"
+        :key="index"
+        :style="{transform: `rotateY(${360 / imgs.length * index}deg) translateZ(${173.20}px)`}"
+        :src="item"
+        alt="">
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { IPreview } from '@/components/preview/src/types';
+import { ComponentInternalInstance, defineComponent, getCurrentInstance, ref } from 'vue';
 
 export default defineComponent({
   name: 'Home',
-  setup() {
+  setup(props) {
+    const instance = getCurrentInstance()
+    const preview = instance?.appContext.config.globalProperties.$preview
     const modelValue = ref(false)
     const toggleDialog = (status: boolean) => {
       modelValue.value = status
@@ -27,16 +44,49 @@ export default defineComponent({
     }
 
     const beforeClose = (done: Function) => {
-      console.log('before-close')
+      (preview as IPreview)({
+        sources: [
+          'https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2752148155,155093617&fm=26&gp=0.JPG',
+          'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1325995315,4158780794&fm=26&gp=0.JPG',
+        ]
+      }) 
       done()
     }
+
+    const imgs = [
+      require(`@/assets/images/1.jpg`),
+      require(`@/assets/images/2.jpg`),
+      require(`@/assets/images/3.jpg`),
+    ]
 
     return {
       modelValue,
       toggleDialog,
       open,
-      beforeClose
+      beforeClose,
+      imgs
     }
   }
 });
 </script>
+
+<style lang="scss">
+body{
+  perspective: 500px;
+}
+.image-wrapper{
+  position: relative;
+  transform-style: preserve-3d;
+  transition: all .5s;
+  width: 400px;
+  margin: 0 auto;
+  height: 222px;
+  img{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
